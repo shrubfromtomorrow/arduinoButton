e8e30b009413b6692f86d2c9efbc18ef9cf1cb67
orion <orionjandrews@gmail.com>
2023-05-17 21:12:26 -0700

    Forgot to add 3D models to version control :(.

--- audioController/audioController.ino
+++ audioController/audioController.ino
@@ -1,22 +1,3 @@
-/*
-  SD card read/write
- 
- This example shows how to read and write data to and from an SD card file 	
- The circuit:
- * SD card attached to SPI bus as follows:
- ** UNO:  MOSI - pin 11, MISO - pin 12, CLK - pin 13, CS - pin 4 (CS pin can be changed)
-  and pin #10 (SS) must be an output
- ** Mega:  MOSI - pin 51, MISO - pin 50, CLK - pin 52, CS - pin 4 (CS pin can be changed)
-  and pin #52 (SS) must be an output
- ** Leonardo: Connect to hardware SPI via the ICSP header
-
- 
- created   Nov 2010  by David A. Mellis
- modified 9 Apr 2012  by Tom Igoe
- 
- This example code is in the public domain.
- 	 
- */
 
 #include <SPI.h>
 #include <SD.h>
@@ -26,10 +7,6 @@ File myFile;
 
 TMRpcm tmrpcm;
 
-// change this to match your SD shield or module;
-//     Arduino Ethernet shield: pin 4
-//     Adafruit SD shields and modules: pin 10
-//     Sparkfun SD shield: pin 8
 const int chipSelect = 10;
 long randNumber;
 
@@ -38,6 +15,7 @@ void setup() {
   tmrpcm.speakerPin = 9;
 
   Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
 
   delay(1);
 
@@ -46,11 +24,14 @@ void setup() {
     Serial.println("SD fail");
     return;  // don't do anything more if not
   }
-  tmrpcm.setVolume(2);
-  // tmrpcm.play("doom.wav");
+  tmrpcm.setVolume(3);
+  // tmrpcm.play("jupiter.wav");
+  // tmrpcm.loop(1);
 }
 
 void loop() {
+  // Serial.println(digitalRead(2));
+  // delay(1000);
   if (Serial.available()) {
     char command = Serial.read();
     if (command == 's') {  //send the letter p over the serial monitor to start playback
@@ -70,6 +51,33 @@ void loop() {
         tmrpcm.stopPlayback();
       }
       tmrpcm.play("jupiter.wav");
+    }
+    else if (command == 'c') {  //send the letter p over the serial monitor to start playback
+      if (tmrpcm.isPlaying() == 1) {
+        tmrpcm.stopPlayback();
+      }
+      tmrpcm.play("cat.wav");
+    }
+    else if (command == 'p') {  //send the letter p over the serial monitor to start playback
+      if (tmrpcm.isPlaying() == 1) {
+        tmrpcm.stopPlayback();
+      }
+      tmrpcm.play("pipe.wav");
+      // tmrpcm.loop(1); 
+    }
+    else if (command == '-') {  //send the letter p over the serial monitor to start playback
+      if (tmrpcm.isPlaying() == 1) {
+        tmrpcm.pause();
+      }
+      tmrpcm.volume(0);
+      tmrpcm.pause();
+    }
+    else if (command == '+') {  //send the letter p over the serial monitor to start playback
+      if (tmrpcm.isPlaying() == 1) {
+        tmrpcm.pause();
+      }
+      tmrpcm.volume(1);
+      tmrpcm.pause();
     }
   }
 }
